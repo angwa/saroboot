ff53220da5f79c57d7b031d5fe6e4f1de25761e0
Angwa Moses <angwamoses@gmail.com>
2023-02-21 15:31:07 +0100

    format code to PSR-12 standard

--- App/Middleware/Auth.php
+++ App/Middleware/Auth.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Middleware;
+
 use \Firebase\JWT\JWT;
 use App\Helpers\Hash;
 use App\Helpers\Header;
@@ -14,24 +16,24 @@ class Auth
 	public static function authenticate()
 	{
 		if (Header::getAuthorizationHeader() == null) {
-		    //header('HTTP/1.0 400 Bad Request');
-		    self::$status = "No authorization header sent";
-		    return false;
+			//header('HTTP/1.0 400 Bad Request');
+			self::$status = "No authorization header sent";
+			return false;
 		}
-	
+
 		$bearer = Header::getBearerToken();
-	
+
 		$jwt = str_replace("Bearer ", "", $bearer);
 
-		$unChecked = @json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $jwt)[1]))));
+		$unChecked = @json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $jwt)[1]))));
 		$toArray = json_decode(json_encode($unChecked), true);
 
 		//getting id of user from payload to compare key in database
 
-		if(empty($toArray["data"]["id"])){
+		if (empty($toArray["data"]["id"])) {
 			header('HTTP/1.0 401 Unathorized access');
-		    self::$status = "Invalid token";
-		    return false;
+			self::$status = "Invalid token";
+			return false;
 		}
 
 		$userId = $toArray["data"]["id"];
@@ -40,45 +42,40 @@ class Auth
 		$secretKey  = "";
 		$loggedKey = $userModel->checkKey($userId);
 
-		if($loggedKey){
+		if ($loggedKey) {
 			$secretKey = $loggedKey;
 		}
 
-		try{
+		try {
 			$userId = $toArray["data"]["id"];
 
 			$userModel = new UserModel();
 			$secretKey  = "";
 			$loggedKey = $userModel->checkKey($userId);
 
-			if($loggedKey){
+			if ($loggedKey) {
 				$secretKey = $loggedKey;
 			}
 			$token = JWT::decode($bearer, $secretKey, array('HS256'));
 			$now =  time();
-			
+
 			$config = new Config();
-			$env = $env = json_decode(json_encode($config->env),true); 
+			$env = $env = json_decode(json_encode($config->env), true);
 			$serverName = $env["hostname"];
-			if ($token->iss !== $serverName &&
-			    $token->exp < $now)
-			{
-			   header('HTTP/1.1 401 Unauthorized');
-			    self::$status = "Unauthorized access";
-		    	return false;
+			if (
+				$token->iss !== $serverName &&
+				$token->exp < $now
+			) {
+				header('HTTP/1.1 401 Unauthorized');
+				self::$status = "Unauthorized access";
+				return false;
 			}
 			self::$data = json_decode(json_encode($token), true);
 			return true;
-		}
-		catch(\Exception $e)
-		{
+		} catch (\Exception $e) {
 			//header('HTTP/1.0 401 Unauthorized');
-			self::$status= "Invalid token.";
-		    return false;
+			self::$status = "Invalid token.";
+			return false;
 		}
-		
-
 	}
-
-
 }
